d291a7dee0aec3fe32a8414e573b2043b2c5c322
GiovaniLandim06 <giovanilandim06@gmail.com>
2025-04-28 11:53:16 -0300

    Fim da aula de Git

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-       System.out.println("Alô Git!");
+       System.out.println("\t \tJEstatistics");
+       System.out.print("-- Controle de Dados Empresarial --");
+       System.out.print("2025");
 
     }
 }
