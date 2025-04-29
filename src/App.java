748e9d715ccf45e7e4e7d2d0e7876567f907ba09
GiovaniLandim06 <giovanilandim06@gmail.com>
2025-04-29 11:37:35 -0300

    Finalizando aula logica1- OK

--- src/App.java
+++ src/App.java
@@ -1,6 +1,36 @@
+import java.util.Scanner; 
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
-       System.out.println("Alô Git!");
 
+        //Aplicando logica de programação (Aristoteles e George Bollean)
+
+       // Scanner sc = new Scanner(System.in);
+
+        //Exercicio-1: Crie um algoritmo para o JStatistics que leia a opção desejada
+        //"Qual o conjunto de dados utilizado: 1-Populacional 2-Amostral"
+
+        int opcao;
+        JOptionPane.showMessageDialog(null, "O Jogo da Estatistica: JStatistics");
+        opcao = Integer.parseInt(JOptionPane.showInputDialog("Vamos começar o Jogo? \n Escolha a Estratégia: \n 1-Populacional \n 2-Amostral"));
+        JOptionPane.showMessageDialog(null,"Você escolheu: " + opcao , "JStatistics", JOptionPane.INFORMATION_MESSAGE);
+       
+
+       
+        
+        
+    
+
+        String opcaoVariavel = JOptionPane.showInputDialog(null, "Que tipo de variável você deseja cadastrar: \n 1-Quantitativa \n 2-Qualitativa");
+        int opVariavel = Integer.parseInt(opcaoVariavel);
+                JOptionPane.showMessageDialog(null,"Sua escolha foi: " + opVariavel , "JStatistics", JOptionPane.INFORMATION_MESSAGE);
+
+
+
+       
+
+
+        
     }
 }
