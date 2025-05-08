d9644aeef1943bb3a1752ae3ed96d9185fdf5662
GiovaniLandim06 <giovanilandim06@gmail.com>
2025-05-08 11:38:45 -0300

    Fim da Aula 06

--- src/App.java
+++ src/App.java
@@ -1,6 +1,24 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-       System.out.println("Alô Git!");
+import javax.swing.JOptionPane;
 
-    }
+public class App {
+  public static void main(String[] args) throws Exception {
+    // Classe é um conjunto de objetos
+    // Nosso objetivo nesse código é entender a diferença
+    // Entre metodo com o tipo void e metodo com o Tipo String
+
+    Mamifero m1 = new Mamifero();
+    m1.setNomeCientifico("Bos Taurus");
+    System.out.println("Nome cientifico: " + m1.getNomeCientifico());
+
+    //Desafio: Crie mais dois objetos
+
+    Mamifero m2 = new Mamifero();
+    m2.setNomeCientifico("Macropus");
+    System.out.println("Nome cientifico: " + m2.getNomeCientifico());
+
+    Mamifero m3 = new Mamifero();
+    m3.setNomeCientifico("Trichechus");
+    System.out.println("Nome cientifico: " + m3.getNomeCientifico());
+
+  }
 }
---
+++ src/Mamifero.java
@@ -0,0 +1,19 @@
+public class Mamifero {
+
+    private int id;
+    private String nomeCientifico;
+
+ 
+    public String getNomeCientifico() {
+        return nomeCientifico;
+    }
+    public void setNomeCientifico(String nomeCientifico) {
+        this.nomeCientifico = nomeCientifico;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    
+}
