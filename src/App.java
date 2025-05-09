b3506e62b92531bdb7756078a941a48da8397e23
GiovaniLandim06 <giovanilandim06@gmail.com>
2025-05-09 09:47:53 -0300

    Exercicio Numero 2

--- src/App.java
+++ src/App.java
@@ -2,23 +2,41 @@ import javax.swing.JOptionPane;
 
 public class App {
   public static void main(String[] args) throws Exception {
-    // Classe é um conjunto de objetos
-    // Nosso objetivo nesse código é entender a diferença
-    // Entre metodo com o tipo void e metodo com o Tipo String
 
-    Mamifero m1 = new Mamifero();
-    m1.setNomeCientifico("Bos Taurus");
-    System.out.println("Nome cientifico: " + m1.getNomeCientifico());
+    JOptionPane.showMessageDialog(null, "Alô, Jardim!", "Sistema de Jardins", JOptionPane.INFORMATION_MESSAGE);
+    int areaJardim = Integer.parseInt(JOptionPane.showInputDialog(null, "Qual a aréa do seu jardim?(Em metros)"));
 
-    //Desafio: Crie mais dois objetos
 
-    Mamifero m2 = new Mamifero();
-    m2.setNomeCientifico("Macropus");
-    System.out.println("Nome cientifico: " + m2.getNomeCientifico());
+    if (areaJardim <= 30) {
+      JOptionPane.showMessageDialog(null, "Seu Jardim é pequeno", "Sistema de Jardins",
+          JOptionPane.INFORMATION_MESSAGE);
+    } else if (areaJardim > 30 && areaJardim <= 50) {
+      JOptionPane.showMessageDialog(null, "Seu Jardim é medio", "Sistema de Jardins", JOptionPane.INFORMATION_MESSAGE);
+    } else {
+      JOptionPane.showMessageDialog(null, "Seu Jardim é grande", "Sistema de Jardins", JOptionPane.INFORMATION_MESSAGE);
+    }
 
-    Mamifero m3 = new Mamifero();
-    m3.setNomeCientifico("Trichechus");
-    System.out.println("Nome cientifico: " + m3.getNomeCientifico());
+    String msgServicos = "Qual serviço você deseja? \n1-Jardinagem - R$400,00 \n2-Adubação - R$300,00 \n3-Fertilização - R$350,00";
+    int servicos = Integer.parseInt(JOptionPane.showInputDialog(null, msgServicos));
 
+    switch (servicos) {
+      case 1:
+        JOptionPane.showMessageDialog(null,
+            "JARDINAGEM \nVamos jardinar toda a sua aréa da forma que você desejar \n3 Dias de serviço",
+            "Sistema de Jardins", JOptionPane.INFORMATION_MESSAGE);
+        break;
+      case 2:
+        JOptionPane.showMessageDialog(null,
+            "ADUBAÇÃO \nVamos levar nosso melhor adubo para ajudar suas plantas a se desenvolverem da melhor forma \n1 Dia de serviço",
+            "Sistema de Jardins", JOptionPane.INFORMATION_MESSAGE);
+        break;
+        case 3: 
+        JOptionPane.showMessageDialog(null, "FERTILIZAÇÃO \nVamos fertilizar todas as suas plantas \n2 Dias de Serviço", "Sistema de Jardins", JOptionPane.INFORMATION_MESSAGE);
+       break;
+
+      default:
+      JOptionPane.showMessageDialog(null, "ERRO \nServiço incorreto", "Sistema de Jardins", JOptionPane.INFORMATION_MESSAGE);
+        break;
+    }
   }
 }
