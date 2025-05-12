95aaf897e5b86a60d58a81ff05bc7cfa8000ca8f
GiovaniLandim06 <giovanilandim06@gmail.com>
2025-05-12 08:52:33 -0300

    Exercicio número 05

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 import javax.swing.JOptionPane;
 
 public class App {
+  
   public static void main(String[] args) throws Exception {
   
     String nomeCliente = JOptionPane.showInputDialog(null, "Qual o seu nome?");
@@ -9,12 +10,33 @@ public class App {
     int largura = Integer.parseInt(JOptionPane.showInputDialog(null, "Qual a largura do seu jardim?"));
     int comprimento = Integer.parseInt(JOptionPane.showInputDialog(null, "Qual o comprimento do seu jardim?"));
 
-    JOptionPane.showMessageDialog(null, nomeCliente, cpf, comprimento);
- 
+    String dadosUsuarios = "Seu nome é: " + nomeCliente + "\nSeu CPF é: " + cpf +  "\nA área do seu jardim é: " + calcularArea(largura, comprimento); 
+    JOptionPane.showMessageDialog(null, dadosUsuarios, "Sistema de Jardins", JOptionPane.INFORMATION_MESSAGE);
+
+    int n = Integer.parseInt(JOptionPane.showInputDialog("Quantos numeros deseja armazenar no array?"));
+
+    int[] vetor = new int[n];
+
+    for (int i = 0; i < vetor.length; i++) {
+     int valor = Integer.parseInt(JOptionPane.showInputDialog("Qual o valor que deseja armazenar na posição " + i));
+     vetor[i] = valor;
+    }
+
+    JOptionPane.showMessageDialog(null, "A media dos valores do seu array é: " + calcularMedia(vetor), "Sistema de Jardins", JOptionPane.INFORMATION_MESSAGE);
+  
   }
 
-  public int calcularArea(int largura, int comprimento){
+  public static int calcularArea(int largura, int comprimento){
     return largura * comprimento;
   }
 
+  public static int calcularMedia(int[] array){
+    int soma = 0;
+    for (int i : array) {
+      soma += i;
+    }
+
+    return soma / array.length;
+
+  }
 }
